f11203617400d64a40d1fbf19cf4bbfc7cee7fe0
Gustavo Xavier <gustavo.xavier@timipro.com.br>
2024-08-25 13:01:21 -0300

    ajuste link imovel

--- obj/Debug/net7.0/TemplateBlazor.AssemblyInfo.cs
+++ obj/Debug/net7.0/TemplateBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemplateBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1ddf55d00963ba06adf4ab67c7c86e9092d64b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65f40a839c62a6f97837178b395a3a9577f64a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemplateBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemplateBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
